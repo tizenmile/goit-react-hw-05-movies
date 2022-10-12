c16753dd104583cac8fecc4e24ad365f80dd455f
tizenmile <tizen48@gmail.com>
2022-10-12 16:02:50 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,9 +15,9 @@ export const App = () => {
       </div>
       <div>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} >
+          <Route path="/goit-react-hw-05-movies" element={<Home />} />
+          <Route path="/goit-react-hw-05-movies/movies" element={<Movies />} />
+          <Route path="/goit-react-hw-05-movies/movies/:movieId" element={<MovieDetails />} >
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
