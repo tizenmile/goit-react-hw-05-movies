0f681f486e0528313a9fbb328995983ba0871106
tizenmile <tizen48@gmail.com>
2022-10-12 16:36:01 +0000

    Deploying to gh-pages from @ tizenmile/goit-react-hw-05-movies@ba20cf968dafe3d48fb779364da4790b7c96b99a 🚀

---
+++ static/js/186.20918efa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[186],{186:function(e,i,n){n.r(i);var r=n(739),s=n(184);i.default=function(e){var i=(0,e.GetMovieReviews)((0,r.UO)().movieId);if(0!==i.length)return(0,s.jsx)("div",{children:(0,s.jsx)("ul",{children:0===i.results.length?(0,s.jsx)("p",{children:"no reviews here"}):i.results.map((function(e){return(0,s.jsxs)("li",{children:[(0,s.jsx)("h2",{children:e.author}),(0,s.jsx)("p",{children:e.content})]},e.id)}))})})}}}]);
+//# sourceMappingURL=186.20918efa.chunk.js.map
